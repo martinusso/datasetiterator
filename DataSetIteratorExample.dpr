a566d23996ac5c73496bcd2cf58a20ef16b48326
magnomp <magnomp@gmail.com>
2011-08-12 09:14:43 -0300

    Adding files

---
+++ DataSetIteratorExample.dpr
@@ -0,0 +1,16 @@
+program DataSetIteratorExample;
+
+uses
+  Forms,
+  Unit1 in 'Unit1.pas' {Form1},
+  UDataSetIterator in 'UDataSetIterator.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  ReportMemoryLeaksOnShutdown := True;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
